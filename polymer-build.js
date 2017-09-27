7561c0a2abdd995d339282a94ccac0ab72be54f9
Ryan Sweet <ryan.sweet@d2l.com>
2017-09-27 15:30:08 -0400

    Replace activity exemptions with d2l-simple-overlay

--- polymer-build.js
+++ polymer-build.js
@@ -6,10 +6,10 @@ const project = new PolymerProject({
 	"entrypoint": "web-components/index.html",
 	"fragments": [
 		"bsi.html",
-		"d2l-activity-exemptions.html",
 		"d2l-image-banner-overlay.html",
 		"d2l-more-less.html",
 		"d2l-my-courses.html",
+		"d2l-simple-overlay.html",
 		"d2l-scroll-spy.html",
 		"d2l-table.html"
 	],

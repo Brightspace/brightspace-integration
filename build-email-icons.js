3cb30f95ca1bfbaa5245bc36be68e9c34144a291
Adam Palaniuk <Adam.Palaniuk@D2L.com>
2017-12-07 09:43:41 -0600

    Add email icons required for Portfolio

--- build-email-icons.js
+++ build-email-icons.js
@@ -5,6 +5,12 @@ const svg2png = require("svg2png");
 	'tier3/assignments',
 	'tier3/quizzing',
 	'tier3/play',
+	'tier3/image',
+	'tier2/file-audio',
+	'tier2/file-document',
+	'tier2/file-video',
+	'tier2/google-drive',
+	'tier2/one-drive',
 	'tier1/bullet'
 ].forEach( filePath =>
 	fs.readFile(`bower_components/d2l-icons/images/${filePath}.svg`)

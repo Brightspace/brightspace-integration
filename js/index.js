726299a38a41262594927df58fbb40a8bf66c596
Dave Lockhart <dave.lockhart@d2l.com>
2018-12-12 16:55:21 -0500

    Creating a queue for callbacks interested in being called after WebComponentsReady has fired

--- js/index.js
+++ js/index.js
@@ -7,6 +7,10 @@ if (typeof Promise === 'undefined') {
 
 window.D2L = window.D2L || {};
 
+import webComponentsReady from './d2l-web-components-ready.js';
+webComponentsReady.init();
+window.D2L.WebComponentsReady = webComponentsReady.WebComponentsReady;
+
 import FastDom from './d2l-fastdom.js';
 window.D2L.FastDom = FastDom;
 

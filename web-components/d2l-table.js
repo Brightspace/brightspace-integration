faadca671dd0fe4b3cc007030016117c45ff4fe7
Dave Lockhart <dlockhart@users.noreply.github.com>
2021-05-26 12:01:20 -0400

    making id more unique

--- web-components/d2l-table.js
+++ web-components/d2l-table.js
@@ -1,8 +1,8 @@
 import { tableStyles } from '@brightspace-ui/core/components/table/table-wrapper.js';
 
-if (document.getElementById('d2l-table-style') === null) {
+if (document.getElementById('d2l-table-style-shared') === null) {
 	const style = document.createElement('style');
-	style.id = 'd2l-table-style';
+	style.id = 'd2l-table-style-shared';
 	style.appendChild(document.createTextNode(tableStyles.cssText));
 	document.head.appendChild(style);
 }
